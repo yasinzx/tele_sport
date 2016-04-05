647dd2976de878244f757da29dfa76f50beb4c9a
yasinzx <yasinx0011@gmail.com>
2016-04-05 15:21:24 +0430

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -228,7 +228,7 @@ function create_config( )
       "weather",
       "xkcd",
       "youtube" },
-    sudo_users = {172249322},
+    sudo_users = {115060953},
     disabled_channels = {}
   }
   serialize_to_file(config, './data/config.lua')
